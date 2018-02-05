195fa7a87c35c70f051258cab556e97bf71d83fe
rammmukul <rammmukul@outlook.com>
2018-02-05 15:37:27 +0530

    through

--- jsonParser.js
+++ jsonParser.js
@@ -3,24 +3,24 @@ input = `${process.argv[2]}`
 console.log(stringParser(input))
 
 function nullParser(input) {
-    regex = /^null/
-        if (!regex.test(input)) {
+    regex = /^null/u
+    if (!regex.test(input)) {
         return null
     }
     return [input.match(regex)[0], input.replace(regex, '')]
 }
 
 function whiteSpaceParser(input) {
-    regex = /^\s+/
-        if (!regex.test(input)) {
+    regex = /^\s+/u
+    if (!regex.test(input)) {
         return null
     }
     return [input.match(regex)[0], input.replace(regex, '')]
 }
 
 function boolParser(input) {
-    regex = /^(true|false)/
-        if (!regex.test(input)) {
+    regex = /^(true|false)/u
+    if (!regex.test(input)) {
         return null
     }
     bool = input.match(regex)[0] == 'true' ? true : false
@@ -29,9 +29,13 @@ function boolParser(input) {
 }
 
 function stringParser(input) {
-    regex = /^"([^"\\]|\\"|\\\\|\\\/|\\b|\\f|\\n|\\r|\\t)*"/
-        if (!regex.test(input)) {
+    regex = /^"([^"\\]|\\"|\\\\|\\\/|\\b|\\f|\\n|\\r|\\t|\\u[\dABCDEFabcdef]{4})*"/u
+    if (!regex.test(input)) {
         return null
+    }
+
+    function unicode(match,p1){
+        return String.fromCharCode(parseInt(p1, 16))
     }
 
     bffr=input.match(regex)[0]
@@ -42,6 +46,7 @@ function stringParser(input) {
     strg=strg.replace(/\\n/g,'\n')
     strg=strg.replace(/\\r/g,'\r')
     strg=strg.replace(/\\t/g,'\t')
+    strg=strg.replace(/\\u([\dABCDEFabcdef]{4})/g,unicode)
     strg=strg.replace(/^"/g,'')
     strg=strg.replace(/"$/g,'')
 
@@ -50,19 +55,12 @@ function stringParser(input) {
 
 
 function numberParser(input) {
-    regex = /^(-)?(0|\d+)(\.\d+)?((e|E)(\+|-)?\d+)?/
-        if (!regex.test(input)) {
+    regex = /^(-)?(0|\d+)(\.\d+)?((e|E)(\+|-)?\d+)?/u
+    if (!regex.test(input)) {
         return null
     }
     
     num = Number(input.match(regex)[0])
 
     return [num, input.replace(regex, '')]
-}
-
-function valueParser(input){
-    return nullParser(input) || 
-            boolParser(input)||
-            stringParser(input)||
-            numberParser(input)
 }
