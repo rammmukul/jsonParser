051f6ea665d94a695adaaa646a2185e316254a22
rammmukul <rammmukul@outlook.com>
2018-02-07 15:53:26 +0530

    objectParser complete

--- jsonParser.js
+++ jsonParser.js
@@ -1,5 +1,27 @@
-let toParse = `${process.argv[2]}`
-console.log(valueParser(toParse))
+// let toParse = `${process.argv[2]}`
+// console.log(whiteSpaceParser(toParse))
+let fs = require('fs')
+let file = './twitter.json'
+fs.readFile(file, 'utf-8', function read(error, str) {
+  if (error) throw error
+  console.log(str);
+  let result = JSON.stringify(valueParser(str))
+  console.log('\nFINAL RESULT : ', result);
+});
+
+
+function valueParser(input){
+    if(whiteSpaceParser(input)){
+        input=whiteSpaceParser(input)[1]
+    }
+
+    return arrayParser(input)||
+            objectParser(input)||
+            boolParser(input)||
+            stringParser(input)||
+            numberParser(input)||
+            nullParser(input)
+}
 
 function nullParser(input) {
     if (input.substring(0,4)!='null') {
@@ -16,7 +38,7 @@ function whiteSpaceParser(input) {
         return null
     }
 
-    return [match[0], input.slice(match[0].length)]
+    return [match[0], input.replace(regex,'')]
 }
 
 function boolParser(input) {
@@ -32,7 +54,7 @@ function boolParser(input) {
 }
 
 function stringParser(input) {
-    let regex = /^"([^"\\]|\\")*"/
+    let regex = /^"([^"\\]|\\"|\\n)*"/
     let match = input.match(regex)
     if (!match) {
         return null
@@ -41,8 +63,8 @@ function stringParser(input) {
     let bffr=match[0]
     let strg=""
     let prevEscape=false
-    for(let i=1; i<bffr.length && bffr[i]!='"' && !prevEscape; i++){
-        strg+=bffr[i]
+    for(let i=1; i<bffr.length && (bffr[i]!='"' || !prevEscape); i++){
+        strg += prevEscape ? '"' : bffr[i]
         prevEscape=bffr[i]=='\\'
     }
 
@@ -60,15 +82,6 @@ function numberParser(input) {
     let num = Number(match[0])
 
     return [num, input.slice(match[0].length)]
-}
-
-function valueParser(input){
-
-    return boolParser(input)||
-            stringParser(input)||
-            numberParser(input)||
-            arrayParser(input)||
-            nullParser(input)
 }
 
 function commaParser(input){
@@ -133,4 +146,87 @@ function arrayParser(isArray){
     isArray=closeSquareParser(isArray)[1]
 
     return [arr,isArray]
+}
+
+function objectParser(isObject){
+    if (!openCurlyParser(isObject)) {
+        return null
+    }
+
+    function openCurlyParser(input){
+        let openSquare = /^\{/
+        let match = input.match(openSquare)
+        if (!match) {
+            return null
+        }
+        
+        return [match[0], input.slice(match[0].length)]
+    }
+    
+    function closeCurlyParser(input){
+        let closeSquare = /^\}/
+        let match = input.match(closeSquare)
+        if (!closeSquare.test(input)) {
+            return null
+        }
+        
+        return [match[0], input.slice(match[0].length)]
+    }
+
+    function colenParser(input){
+        let colen = /^:/
+        let match = input.match(colen)
+        if (!match) {
+            return null
+        }
+        
+        return [match[0], input.slice(match[0].length)]
+    }
+    
+    let obj={}
+    isObject=openCurlyParser(isObject)[1]
+
+    while(!closeCurlyParser(isObject)){
+
+        if(whiteSpaceParser(isObject)){
+            isObject=whiteSpaceParser(isObject)[1]
+        }
+        
+        let key = stringParser(isObject)[0]
+        isObject = stringParser(isObject)[1]
+
+        if(whiteSpaceParser(isObject)){
+            isObject=whiteSpaceParser(isObject)[1]
+        }
+
+        isObject=colenParser(isObject)[1]
+
+        if(whiteSpaceParser(isObject)){
+            isObject=whiteSpaceParser(isObject)[1]
+        }
+
+        let value = ''
+        if(valueParser(isObject)){
+            value = valueParser(isObject)[0]
+            isObject = valueParser(isObject)[1]
+        }
+
+        obj[key]=value
+
+        if(whiteSpaceParser(isObject)){
+            isObject=whiteSpaceParser(isObject)[1]
+        }
+
+        if(commaParser(isObject)){
+            isObject=commaParser(isObject)[1]
+            continue
+        }
+        break
+    }
+
+    if(closeCurlyParser(isObject)){
+        isObject=closeCurlyParser(isObject)[1]
+    }
+
+    return [obj,isObject]
 }
