e07fcf67ced47d618ad48cfb09fff377bc5a5335
rammmukul <rammmukul@outlook.com>
2018-02-12 08:49:25 +0530

    Add jest tests and move sample examples

--- jsonReader.js
+++ jsonReader.js
@@ -2,19 +2,13 @@ let fs = require('fs')
 let file = `${process.argv[2]}`
 let parser = require('./jsonParser.js')
 fs.readFile(file, 'utf-8', (error, str) => {
-    if (error) {
-      throw error
-    }
-    console.log('String:\n', str)
-    console.log('\n\n\n')
-    console.log('*******************************************')
-    console.log('\n\n\n')
-    let result
-    try {
-      result = JSON.stringify(parser.parseJSON(str)[0], null, 2)
-    } catch (e) {
-      throw Error('Invalid JSON')
-    }
-    console.log('\nFINAL RESULT :\n', result)
-  })
-  
+  if (error) {
+    throw error
+  }
+  console.log('String:\n', str)
+  console.log('\n\n\n')
+  console.log('*******************************************')
+  console.log('\n\n\n')
+  let result = JSON.stringify(parser.parseJSON(str)[0], null, 2)
+  console.log('\nFINAL RESULT :\n', result)
+})
